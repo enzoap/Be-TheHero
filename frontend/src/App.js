7f2e3ecd299ee252dacf6a6b2f2fbf454b69dfa6
Enzo <enzoap2009@hotmail.com>
2020-03-27 19:57:16 -0300

    Front-end do app.

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+
+import './global.css'
+
+import Routes from './routes'
+import { Route } from 'react-router-dom';
+//JSX (Javascript, Html rodando no javascript)
+
+function App() {
+  return (
+    <Routes></Routes>
+  )
+}
+
+export default App;
